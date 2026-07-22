dcd6ad1033313e4b0f8a995a11295237b69dfbdd
kjurkows <kjurkows@student.42warsaw.pl>
2026-07-22 15:15:13 +0200

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 if [ -n "$1" ] && [ "$1" = "bonus" ]; then
+	if [ -d ft_printf ]; then
+		make -C ft_printf fclean > /dev/null
+		if [ $? -ne 0 ]; then
+			echo "Build failed."
+			exit 1
+		fi
+	fi
 	make bonus > /dev/null
 	if [ $? -ne 0 ]; then
 		echo "Build failed."
 		exit 1
 	fi
 else
+	if [ -d ft_printf ]; then
+		make -C ft_printf fclean > /dev/null
+		if [ $? -ne 0 ]; then
+			echo "Build failed."
+			exit 1
+		fi
+	fi
 	make > /dev/null
 	if [ $? -ne 0 ]; then
 		echo "Build failed."
