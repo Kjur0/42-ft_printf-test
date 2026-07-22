540e9c76431888633bd2821a4b28201dc99db008
kjurkows <kjurkows@student.42warsaw.pl>
2026-07-22 15:19:50 +0200

    Fix test.sh v2

--- test.sh
+++ test.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kjurkows <kjurkows@student.42warsaw.pl>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/07/22 15:18:03 by kjurkows          #+#    #+#              #
+#    Updated: 2026/07/22 15:18:19 by kjurkows         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+PRINTF_DIR=ft_printf
 
 if [ -n "$1" ] && [ "$1" = "bonus" ]; then
-	if [ -d ft_printf ]; then
-		make -C ft_printf fclean > /dev/null
+	if [ -d $PRINTF_DIR ]; then
+		make -C $PRINTF_DIR all > /dev/null
 		if [ $? -ne 0 ]; then
 			echo "Build failed."
 			exit 1
@@ -14,8 +27,8 @@ if [ -n "$1" ] && [ "$1" = "bonus" ]; then
 		exit 1
 	fi
 else
-	if [ -d ft_printf ]; then
-		make -C ft_printf fclean > /dev/null
+	if [ -d $PRINTF_DIR ]; then
+		make -C $PRINTF_DIR bonus > /dev/null
 		if [ $? -ne 0 ]; then
 			echo "Build failed."
 			exit 1
@@ -30,4 +43,5 @@ fi
 
 ./ft_printf-test
 
+make -C $PRINTF_DIR fclean > /dev/null
 make fclean > /dev/null
